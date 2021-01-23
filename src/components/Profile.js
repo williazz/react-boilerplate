63cfb2534c96bb467ee69ac69f11bd45a1296647
William Zhou <williamz.zhou1@gmail.com>
2021-01-23 08:45:58 -0800

    Completed "Calling an API" quickstart

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,17 +1,66 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
+import { auth0 } from '../secrets';
 
 const Profile = () => {
-  const { user, isLoading, isAuthenticated } = useAuth0();
+  const {
+    user,
+    isAuthenticated,
+    getAccessTokenSilently,
+    isLoading,
+  } = useAuth0();
+  const [userMetadata, setUserMetadata] = useState(null);
+
+  useEffect(() => {
+    const getUserMetadata = async () => {
+      if (isLoading || !isAuthenticated) return;
+      const { domain } = auth0;
+
+      try {
+        const accessToken = await getAccessTokenSilently({
+          audience: `https://${domain}/api/v2/`,
+          scope: 'read:current_user',
+        });
+
+        const userDetailsByIdUrl = `https://${domain}/api/v2/users/${user.sub}`;
+
+        const metadataResponse = await fetch(userDetailsByIdUrl, {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        });
+
+        /* eslint-disable */
+        const { user_metadata } = await metadataResponse.json();
+        setUserMetadata(user_metadata);
+        console.log({ userDetailsByIdUrl, user_metadata, accessToken });
+      } catch (e) {
+        console.error('Ruh roh:', e.message);
+      }
+    };
+
+    getUserMetadata();
+  }, []);
+  /* eslint-enable */
+
   if (isLoading) {
-    return <h2>Loading...</h2>;
+    return <div>Loading ...</div>;
   }
+
   return (
     isAuthenticated && (
       <div>
         <img src={user.picture} alt={user.name} />
-        <h2>Name: {user.name}</h2>
+        <p>Name: {user.name}</p>
         <p>Email: {user.email}</p>
+        <p>
+          User Metadata:{' '}
+          {userMetadata ? (
+            <pre>{JSON.stringify(userMetadata, null, 2)}</pre>
+          ) : (
+            'No user metadata defined'
+          )}
+        </p>
       </div>
     )
   );
