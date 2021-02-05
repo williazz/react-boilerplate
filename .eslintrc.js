734f254affb27bf77563ae4422c98ae6605d47cd
William Zhou <williamz.zhou1@gmail.com>
2021-02-05 09:45:07 -0800

    Config change

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,7 +24,7 @@ module.exports = {
     'react/react-in-jsx-scope': 'off',
     'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
     'react/jsx-closing-bracket-location': [1, 'after-props'],
-    'no-console': ['error', { allow: ['warn', 'error'] }],
+    'no-console': [1, { allow: ['warn', 'error'] }],
     'react-hooks/rules-of-hooks': 'error', // Checks rules of Hooks
     'react-hooks/exhaustive-deps': 'warn', // Checks effect dependencies
   },
