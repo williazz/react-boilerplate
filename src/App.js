0f3fe777c1a4022e4ee8f112b155e954216e6cf4
William Zhou <williamz.zhou1@gmail.com>
2021-02-09 17:50:15 -0800

    Saving progress

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
 import './App.css';
-import { Auth0Provider } from '@auth0/auth0-react';
-import { AUTH0 } from './config';
 import Profile from './components/Profile';
 import Login from './components/LoginButton';
 import Logout from './components/LogoutButton';
 
 function App() {
   return (
-    <Auth0Provider
-      domain={AUTH0.DOMAIN}
-      clientId={AUTH0.CLIENT_ID}
-      redirectUri={window.location.origin}>
+    <div>
       <Profile />
       <Login />
       <Logout />
-    </Auth0Provider>
+    </div>
   );
 }
 
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,17 +1,52 @@
+import React, { useEffect, useState } from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
-import React from 'react';
 
 const Profile = () => {
-  const { user, isAuthenticated, isLoading } = useAuth0();
+  const { user, isAuthenticated, getAccessTokenSilently } = useAuth0();
+  const [userMetadata, setUserMetadata] = useState(null);
+  useEffect(() => {
+    const getUserMetadata = async () => {
+      const domain = 'williazz.us.auth0.com';
 
-  if (isLoading) return <p>Loading profile... </p>;
+      try {
+        const accessToken = await getAccessTokenSilently({
+          audience: `https://${domain}/api/v2/`,
+          scope: 'read:current_user',
+        });
+
+        const userDetailsByIdUrl = `https://${domain}/api/v2/users/${user.sub}`;
+
+        console.log({ accessToken });
+
+        const metadataResponse = await fetch(userDetailsByIdUrl, {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        });
+
+        const { user_metadata } = await metadataResponse.json();
+
+        setUserMetadata(user_metadata);
+      } catch (e) {
+        console.log(e.message);
+      }
+    };
+
+    getUserMetadata();
+  });
 
   return (
     isAuthenticated && (
       <div>
         <img src={user.picture} alt={user.name} />
-        <p>Name: {user.name}</p>
-        <p>Email: {user.email}</p>
+        <h2>{user.name}</h2>
+        <p>{user.email}</p>
+        <h3>User Metadata</h3>
+        {userMetadata ? (
+          <pre>{JSON.stringify(userMetadata, null, 2)}</pre>
+        ) : (
+          'No user metadata defined'
+        )}
       </div>
     )
   );
